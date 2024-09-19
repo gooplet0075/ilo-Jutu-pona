a72f24279eb400d995617a900476ec7f5ab7a6ae
gooplet0075 <79529592+gooplet0075@users.noreply.github.com>
2024-09-19 14:16:18 -0400

    fixed bugs, added readme

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# ilo-Jutu-pona
-Chrome extension for YouTube Live chat
+# ilo Jutu pona la lipu pi pana sona (instructions Chrome extension for YouTube Live chat chrome extension)
+
+## Features
+The Chrome extension automatically formats Toki Pona messages in Sitelen Pona using nasin-nanpa.otf. It is permissive of many tp word variations, allows for custom YouTube emotes, and formats any unofficial 'proper words' with Comic Sans. 
+
+To better display chat messages for incorporation into live-stream overlays, profile pictures are removed, as well as additional padding around individual messages. Deleted messages are removed altogether instead of censored with `[message retracted]`.
+
+<img src="./readme-images//example.png" alt="chat example" width=300>
+
+## Installing
+1. Download the source code.
+2. You'll need to have Chrome installed; navigate to `chrome://extensions/`. 
+3. Toggle on "Developer mode" in the top-right. 
+4. Click on "Pack extension". Leave blank the optional "Private key file", and just browse for the "Extension root directory", which is wherever you downloaded this extension's code to. It probably is in a directory ending with `...\ilo-Jutu-pona`. Click "Pack extension" to confirm. 
+5. Click "Load unpacked", selecting the same directory. 
+
+When properly loaded, you should see this on the chrome extensions page:
+
+<img src="./readme-images/active-extension.png" alt="active extension" width=350>
+
+## Using the extension
+The extension only works on the popout chat window. Click the 3-dot menu in the top right, and select "popout chat". 
+
+<img src="./readme-images/popout-chat.png" alt="popout chat" width=350>
+
+(Tip: you can popout chat before going live by navigating to the broadcast in YouTube studio).
+
+Alternatively, you can navigate directly to the "popout" chat page if you have your broadcast's video ID: `https://studio.youtube.com/live_chat?is_popout=1&v=VIDEOID`
+
+Since you are still viewing chat in a browser, you can easily increase and decrease the font size to get the best possible appearance and legibility for your purposes using `ctrl-shift-[ + or - ]`, or simply holding `ctrl` and scrolling. 
+
+## Incorporating chat into OBS
+In the Sources panel in OBS, click the + button and select "Window capture"
+
+<img src="./readme-images//obs1.png" alt="obs sources" width=300>
+
+Give it a unique and easily-recognizable name. 
+
+<img src="./readme-images/obs2.png" alt="obs window capture" width=300>
+
+Select the chat window. You can resize the chat browser itself so it displays exactly the amount of chat that you'd like. 
+
+<img src="./readme-images/obs3.png" alt="window capture settings">
+
+You've added the chat to your OBS scene! You can now resize, move, and crop it as you like. Hold `alt` and drag the resize handles around the image to easily crop the top and bottom of the chat window.
+
+<img src="./readme-images/obs4.png" alt="cropping source in OBS" width=200>
+
+For greatest clarity, I'd recommend not resizing the OBS object but rather the Chrome browser window itself, and adjusting text size there. Just crop the window appropriately in OBS, and you're set!
+
+<img src="./readme-images/obs5.png" alt="cropped source" width=200>
+
+## Credits
+I'd like to thank jan Kekan San for all his input throughout the process of creating this extension!! His insights into detecting and formatting toki pona messages were invaluable. 
+
+This project uses [nasin-nanpa-3.1.0.otf](https://github.com/ETBCOR/nasin-nanpa).
+
+is-toki-pona.js was generously contributed by [m4ym4y](https://github.com/m4ym4y) since I was clueless about regex.
+
+### Bonus
+Here are some pictures of my cats Denna and Kvothe as well as Henry the skull with a corncob pipe
+<img src="./readme-images/henry-corncob-pipe.jpeg" alt="Henry the skull w/ a corncob pipe and no button nose" width=200>
+<img src="./readme-images/denna.jpg" alt="Denna the cat" width=200>
+<img src="./readme-images/kvothe.jpg" alt="Kvothe the cat" width=250>
--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 const observer = new MutationObserver(async function(mutations_list) {
 	mutations_list.forEach(async function(mutation) {
+        if (mutation.attributeName === "is-deleted") {
+            mutation.target.style.display = 'none';
+        };
 		mutation.addedNodes.forEach(async function(added_node) {
-			if(added_node.tagName == 'YT-LIVE-CHAT-TEXT-MESSAGE-RENDERER') {
-                console.log('added_node',added_node)
-                element = document.getElementById(added_node.id);
-                console.log('element',element);
-                removeProfilePicture(element);
-                formatMessage(element);
+			if((added_node.tagName == 'YT-LIVE-CHAT-TEXT-MESSAGE-RENDERER') && (added_node.parentNode.id === 'items')) {
+                await removeProfilePicture(added_node);
+                await formatMessage(added_node);
 			}
 		});
 	});
 });
 
-async function formatMessage(element) {
-    let message = element.querySelector('#message');
+async function formatMessage(element) { // formats tp words w/ sitelen pona
+    element.style.padding = '0px';
+    const message = element.querySelector('#message');
     const messageText = 
         Array.from(message.childNodes)
         .filter((segment) => segment.nodeType === Node.TEXT_NODE)
@@ -22,40 +23,42 @@ async function formatMessage(element) {
         .replace(/\s+/g,' ')
         .trim();
     if (await isTokiPona(messageText)) {
+        console.log(message.innerHTML)
+        let properNouns = [];
         for (const word of messageText.split(' ')) {
             let newWord;
             if ((word[0] != word[0].toLowerCase()) && (word.substring(1) == word.substring(1).toLowerCase())) {
-                newWord = `<span class="propernoun">${word}</span>`;
+                if (!properNouns.includes(newWord)) {
+                    newWord = `<span class="propernoun">${word}</span>`;
+                    properNouns.push(newWord);
+                    message.innerHTML = message.innerHTML.replaceAll(word, newWord);
+                };
             } else {
                 newWord = word.toLowerCase().replace(/(.)\1*/g, '$1');
+                message.innerHTML = message.innerHTML.replace(word, newWord);
             };
-            message.innerHTML = message.innerHTML.replace(word, newWord);
         }; 
         message.style.fontFamily = 'nasin-nanpa';
     }
 }
 
-async function removeProfilePicture(element) {
-    let profilePicture = element.querySelector('yt-img-shadow');
-    profilePicture.remove(); //removes pics from existing messages
+async function removeProfilePicture(element) { // hides profile picture from message
+    const profilePicture = element.querySelector('yt-img-shadow');
+    profilePicture.style.display='none'; 
 }
 
 async function initialize() {
-    var itemScroller = document.getElementById('item-scroller');
-    itemScroller.style.overflow = 'hidden'; //removes scrollbar
-
-    deletedElement = document.querySelector('yt-live-chat-viewer-engagement-message-renderer');
-    deletedElement.remove(); //removes that annoying card that's in chat when you join
-
+    document.querySelector('yt-live-chat-viewer-engagement-message-renderer').style.display = 'none';
     initElements = document.getElementsByClassName('style-scope yt-live-chat-item-list-renderer')[5];
     existingMessages = initElements.getElementsByTagName('yt-live-chat-text-message-renderer');
-
     for (message of existingMessages) {
         removeProfilePicture(message);
         formatMessage(message);
     };
 }
 
-
 const init = initialize();
-observer.observe(document.getElementsByClassName('style-scope yt-live-chat-item-list-renderer')[5], { subtree: false, childList: true });
+observer.observe(document.getElementsByClassName('style-scope yt-live-chat-item-list-renderer')[5], { subtree: true, childList: true, attributes: true });
+
+
+//mi wile e ni: jan ale liiiii utala ala.:elbowcough: utala liii ike lon tenpo ale. mi jan pi ma Mewika.:hydrate: taso jan lawa pi ma Mewika pilin ala e ni: jan ale o utala :face-orange-frowning:ala. ni< liiiiiiii ike tawa mi.
